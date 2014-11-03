09d458bb695aac6099115c2d1a32f00e63d20ab5
eddiegemayel <eddiegem16@gmail.com>
2014-11-03 18:42:34 -0500

    Made a few slight changes

--- grubQuest-node/assets/js/parse.js
+++ grubQuest-node/assets/js/parse.js
@@ -1,9 +1,7 @@
-//REMOVE MSG
-
 var request = require('request');
 
-// Function to find the weather of the location entered
-function getWeather(){
+// Function to find the menu results of the location entered
+function getResults(){
 
 	//get zipcode the user entered
 	var zipcode = '';
@@ -33,7 +31,7 @@ function getWeather(){
 					if(!json.object!=[]){
 
 						//spit back out the information to the user
-						console.log(json.objects[0] + "<br>" + json.objects.street_address[0] + " " + json.objects[0].locality + " " + json.objects[0].postal_code + "<br>" +
+						console.log(json.objects[0] + "<br> Street Address: " + json.objects.street_address[0] + ". Located in " + json.objects[0].locality + " " + json.objects[0].postal_code + "<br>" +
 							"\nCall: " + json.objects[0].phone + "<br>" + json.objects[0].website_url + "<br>" +);
 
 					//if there is an error
@@ -49,8 +47,3 @@ function getWeather(){
 		});
 	}
 }
-
-
-
-
-
