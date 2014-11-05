738e3e2d76ad4cf8f70e1cc910de658bb8f2a71a
eddiegemayel <eddiegem16@gmail.com>
2014-11-05 15:10:00 -0500

    ADDED comments to new code
    trying to get the json data to write to the page properly.

--- grubQuest-node/app.js
+++ grubQuest-node/app.js
@@ -28,7 +28,10 @@ app.get("/", function(req, res){
 	res.render("index", {"Greeting": "Good morning"});
 
 });
+
 app.get('/:page',function(req, res){
+	//GETTING JSON AND PUTTING IT INTO AN ARRAY
+	//STILL CANT GET IT TO WRITE TO THE ACTUAL PAGE
 var locu = require('locu');
 var key = '2834e3e19203329d8c2d1d6208afdd0c44fe2ad6';
 var vclient = new locu.VenueClient(key);
@@ -48,11 +51,10 @@ vclient.search({has_menu: 'True', category: 'restaurant', postal_code: 32792}, f
 app.get("/results", function (req, res){
 	if(fs.existsSync('views/'+req.params.page+'.ejs')){
 
-<<<<<<< HEAD
 		res.render(req.params.page, {message: req.params.id, fullUrl : req.protocol + '://' + req.get('host') + req.originalUrl});
-=======
+
 		});
->>>>>>> FETCH_HEAD
+
 
 	}else{
 		res.render('404: Page not found');
