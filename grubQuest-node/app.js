47e90219612329d6c7eaf92231dc24f71ef33e4c
ncarvalho <nicolecarvalho@fullsail.edu>
2014-11-06 09:19:25 -0500

    Fixed my results function

--- grubQuest-node/app.js
+++ grubQuest-node/app.js
@@ -32,14 +32,14 @@ app.get("/", function(req, res){
 app.get('/:page',function(req, res){
 	//GETTING JSON AND PUTTING IT INTO AN ARRAY
 	//STILL CANT GET IT TO WRITE TO THE ACTUAL PAGE
-var locu = require('locu');
-var key = '2834e3e19203329d8c2d1d6208afdd0c44fe2ad6';
-var vclient = new locu.VenueClient(key);
-var searchVar = [];
-vclient.search({has_menu: 'True', category: 'restaurant', postal_code: 32792}, function(results){
-	searchVar.push(results);
-	console.log("Json: %j", searchVar);
-});
+	var locu = require('locu');
+	var key = '2834e3e19203329d8c2d1d6208afdd0c44fe2ad6';
+	var vclient = new locu.VenueClient(key);
+	var searchVar = [];
+	vclient.search({has_menu: 'True', category: 'restaurant', postal_code: 32792}, function(results){
+		searchVar.push(results);
+		console.log("Json: %j", searchVar);
+	});
 	if(fs.existsSync('views/'+req.params.page+'.ejs')){
 		res.render(req.params.page, {message: req.params.id, fullUrl : req.protocol + '://' + req.get('host') + req.originalUrl});
 
@@ -50,17 +50,10 @@ vclient.search({has_menu: 'True', category: 'restaurant', postal_code: 32792}, f
 
 app.get("/results", function (req, res){
 	if(fs.existsSync('views/'+req.params.page+'.ejs')){
-
 		res.render(req.params.page, {message: req.params.id, fullUrl : req.protocol + '://' + req.get('host') + req.originalUrl});
-
-		});
-
-
 	}else{
 		res.render('404: Page not found');
 	}
-
-
 });
 
 // Start the Server
