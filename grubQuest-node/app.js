e86be69fa513d270404ec7256d2d3b9d7406f9af
eddiegemayel <eddiegem16@gmail.com>
2014-11-05 19:26:35 -0500

    Cleaned up code
    added some comments and cleaned up loose variables.

--- grubQuest-node/app.js
+++ grubQuest-node/app.js
@@ -1,15 +1,16 @@
 var mongojs = require("mongojs"),
 	express = require("express"),
 	engine = require("ejs-locals"),
-	fs = require('fs'),
+	fs = require("fs"),
 	async = require("async"),
 	http = require("http"),
-	locu = require('locu'),
-	port = 4000;
+	locu = require("locu"),			//locu lib
+	key = "2834e3e19203329d8c2d1d6208afdd0c44fe2ad6",	//API key
+	port = 4000;	//port number
 
 
 
-
+//initlaize express
 var app = express();
 
 // Create the http Server
@@ -23,7 +24,7 @@ app.set('view engine', 'ejs');
 app.use('/views', express.static('/views'));
 app.use('/assets', express.static(__dirname + '/assets'));
 
-
+//index route
 app.get("/", function(req, res){
 	res.render("index", {"Greeting": "Good morning"});
 
@@ -32,14 +33,17 @@ app.get("/", function(req, res){
 app.get('/:page',function(req, res){
 	//GETTING JSON AND PUTTING IT INTO AN ARRAY
 	//STILL CANT GET IT TO WRITE TO THE ACTUAL PAGE
-var locu = require('locu');
-var key = '2834e3e19203329d8c2d1d6208afdd0c44fe2ad6';
-var vclient = new locu.VenueClient(key);
-var searchVar = [];
-vclient.search({has_menu: 'True', category: 'restaurant', postal_code: 32792}, function(results){
-	searchVar.push(results);
-	console.log("Json: %j", searchVar);
-});
+	
+	//new client 
+	//initialize with locu method
+	var vclient = new locu.VenueClient(key);
+	//search result objects stored in array
+	var searchVar = [];
+
+	vclient.search({has_menu: 'True', category: 'restaurant', postal_code: 32792}, function(results){
+		searchVar.push(results);
+		console.log("Json: %j", searchVar);
+	});
 	if(fs.existsSync('views/'+req.params.page+'.ejs')){
 		res.render(req.params.page, {message: req.params.id, fullUrl : req.protocol + '://' + req.get('host') + req.originalUrl});
 
@@ -53,9 +57,7 @@ app.get("/results", function (req, res){
 
 		res.render(req.params.page, {message: req.params.id, fullUrl : req.protocol + '://' + req.get('host') + req.originalUrl});
 
-		});
-
-
+		
 	}else{
 		res.render('404: Page not found');
 	}
